6873a204d4d34767278aaddc752e42872b9229cd
joelostblom <joel.ostblom@gmail.com>
2020-01-19 10:57:46 +0100

    Decrease kale copper value as per the latest usda info

--- nutrimap/main.py
+++ nutrimap/main.py
@@ -125,6 +125,7 @@ flowers.loc[flowers['Shrt_Desc'] == 'Semolina,unenriched',
             'Sugar_Tot_(g)'] = 0.7
 flowers.loc[flowers['Shrt_Desc'] == 'Triticale', 'Sugar_Tot_(g)'] = 1
 flowers.loc[flowers['Shrt_Desc'] == 'Triticale', 'Fiber_TD_(g)'] = 14.6
+flowers.loc[flowers['Shrt_Desc'] == 'Kale,raw', 'Copper_(mg)'] = 0.0533
 na_indices = flowers.isnull().any(axis=1).to_numpy().nonzero()
 na_foods = flowers.iloc[na_indices]['Shrt_Desc'].values
 flowers = flowers.dropna()
